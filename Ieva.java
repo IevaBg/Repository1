305b5e6e5696a24c14cff50c5b8c230d026ef0d6
IevaBg <86737962+IevaBg@users.noreply.github.com>
2021-07-07 21:11:43 +0300

    Update Ieva.java

--- Ieva.java
+++ Ieva.java
@@ -2,24 +2,12 @@ public class Ieva {
 
     public static void main (String[] args) {
 
-        String name = "Ieva";
-        byte age = 33;
-        char gender = 'F';
-        short jobsToDo = 350;
-        float timeLeftInH = 1.5F;
-        boolean isItPossible = false;
+        int a = -5 + 8 * 6;
+        int b = (55+9) % 9;
+        int c = 5 + 15 / 3 * 2 - 8 % 3;
 
-        System.out.print("Name: ");
-        System.out.println(name);
-        System.out.print("Age: ");
-        System.out.println(age);
-        System.out.print("Gender: ");
-        System.out.println(gender);
-        System.out.print("Jobs in \"to do list\": ");
-        System.out.println(jobsToDo);
-        System.out.print("Time left to finish the list in hours: ");
-        System.out.println(timeLeftInH);
-        System.out.print("Is it possible?");
-        System.out.println(" " + false + " " + "hell yeah");
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
     }
 }
